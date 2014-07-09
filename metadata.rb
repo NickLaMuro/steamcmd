c113b641d0ca8a93a179f3e46e94b7a1d6045ed1
Marcin Sawicki <odcinek@gmail.com>
2014-07-08 23:05:20 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# Encoding: utf-8
+name             'steamcmd'
+maintainer       'Marcin Sawicki'
+maintainer_email 'odcinek@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures steamcmd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
