186d8484320b941c563a0d5f3dad5cedcf92cd0b
Marcin Sawicki <odcinek@gmail.com>
2014-07-16 13:50:52 -0700

    Updated version

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ maintainer_email 'odcinek@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures steamcmd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
