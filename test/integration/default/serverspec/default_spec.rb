c1ae6baf479b526d4ed33ae2548ad94e83d15016
Nick LaMuro <nicklamuro@gmail.com>
2014-12-23 21:01:20 -0600

    Wrote some basic tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,44 @@
 # Encoding: utf-8
 
-require_relative 'spec_helper'
+require 'spec_helper'
 
 describe 'default' do
-  it { pending 'write some tests' }
+
+  describe user('steam') do
+    it { should exist }
+    it { should belong_to_group 'steam' }
+    it { should have_home_directory '/home/steam' }
+  end
+
+  describe file('/opt/steamcmd') do
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by 'steam' }
+    it { should be_grouped_into 'steam' }
+  end
+
+  describe file('/home/steam/apps') do
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by 'steam' }
+    it { should be_grouped_into 'steam' }
+  end
+
+  describe file('/opt/steamcmd/steam.sh') do
+    it { should be_file }
+  end
+
+  describe file('/opt/steamcmd/steamcmd.sh') do
+    it { should be_file }
+  end
+
+  describe file('/opt/steamcmd/linux32') do
+    it { should be_directory }
+  end
+
+  describe file('/tmp/steamcmd_linux.tar.gz') do
+    it { should_not be_file }
+    it { should_not be_directory }
+  end
+
 end
